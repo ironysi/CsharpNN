6a3f7682c9ac9e4d2179d50340eaf54af2c4cf12
irony <irony@DESKTOP-IRK4754>
2016-12-05 23:41:39 +0100

    Polished that a bit. Created do while loop based on error

--- AI_NeuralNetwork/NNConsole/Program.cs
+++ AI_NeuralNetwork/NNConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Text;
+using System.IO;
+using System.Linq;
 using MyDataSet;
 using NeuralNetworkTesting;
 
@@ -43,39 +44,61 @@ namespace NNConsole
 
         private void RunBCancer()
         {
-            string[] columnTypes = { "numeric", "categorical", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric" };
+            string[] columnTypes =
+            {
+                "numeric", "categorical", "numeric", "numeric", "numeric", "numeric", "numeric",
+                "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric",
+                "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric",
+                "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"
+            };
             int[] outputColumns = { 1 };
             int[] ignoredColumns = { 0 };
-            Data data = new Data("Breasts.txt",30,1,0.8,columnTypes,outputColumns,ignoredColumns);
 
-            double[][] learningInputs = data.LearningInputs.ToRowArrays();
-            double[][] learningOutputs = data.LearningOutputs.ToRowArrays();
-            // initialize with x perception neurons x hidden layer neurons x output neurons and a learning rate
-            double learningRate = 0.01;
-            NeuralNet net = new NeuralNet(30, 30, 1, learningRate);
+            Data data = new Data("Breasts.txt", 30, 1, 0.8, columnTypes, outputColumns, ignoredColumns);
+            NeuralNet net = new NeuralNet(30, 30, 1, 0.005);
 
-            net.Train(learningInputs, learningOutputs, 1000);
-
-            TestNetwork(net, data.TrainingInputs.ToRowArrays(), data.TrainingOutputs.ToRowArrays());
+            RunUntilDesiredError(0.01, 5, data, net);
         }
-
         private void RunIris()
         {
-            //Data data = new Data("Iris.txt", 4, 1, 0.9);
             string[] categories = { "numeric", "numeric", "numeric", "numeric", "categorical" };
-            Data data = new Data("Iris.txt", 4,3, 0.8, categories);
-            double learningRate = 0.11;
-            NeuralNet net = new NeuralNet(4, 4, 3, learningRate);
+            Data data = new Data("Iris.txt", 4, 3, 0.8, categories);
+            NeuralNet net = new NeuralNet(4, 4, 3, 0.01);
 
-            double[][] learningInputs = data.LearningInputs.ToRowArrays();
-            double[][] learningOutputs = data.LearningOutputs.ToRowArrays();
-            net.Train(learningInputs, learningOutputs, 10000);
+            RunUntilDesiredError(0.5, 50, data, net);
+        }
+        private void RunUntilDesiredError(double desiredError, int printErrorEveryXIterations, Data data, NeuralNet net)
+        {
+            double error;
+            int i = 1;
 
-            TestNetwork(net, data.TrainingInputs.ToRowArrays(), data.TrainingOutputs.ToRowArrays());
+            do
+            {
+                net.Train(data.LearningInputs.ToRowArrays(), data.LearningInputs.ToRowArrays(), 10);
+                error = TestNetwork(net, data.TrainingInputs.ToRowArrays(), data.TrainingOutputs.ToRowArrays());
+                i++;
+
+                if (i % printErrorEveryXIterations == 0)
+                {
+                    Console.WriteLine(error);
+                }
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("***************--------------------END OF ITERATION--------------------***************");
+                Console.ResetColor();
+            } while (error > desiredError);
+
+            Console.WriteLine("\nError:{0}\t\t Iterations:{1}", error, i);
+            Console.WriteLine("************FINISHED************");
+            Console.ReadLine();
         }
 
-        private void TestNetwork(INeuralNet net, double[][] trainingInputs, double[][] trainingOutputs)
+
+
+        private double TestNetwork(INeuralNet net, double[][] trainingInputs, double[][] trainingOutputs)
         {
+            double[] error = new double[trainingInputs.Length];
+
+
             for (int i = 0; i < trainingInputs.Length; i++)
             {
                 for (int j = 0; j < trainingInputs[0].Length; j++)
@@ -84,10 +107,32 @@ namespace NNConsole
                 }
 
                 net.Pulse();
+                error[i] = trainingOutputs[i][0] - net.OutputLayer[0].Output;
 
-                Console.WriteLine(trainingOutputs[i][0] + "\t" + net.OutputLayer[0].Output);
-                Console.WriteLine("-----------------------------------------------");
+                if (error[i] < -0.3)
+                {
+
+                    for (int k = 0; k < trainingOutputs[i].Length; k++)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(trainingOutputs[i][k] + "\t\t\t" + net.OutputLayer[0].Output + "\t" + error[i]);
+                        Console.ResetColor();
+                    }
+
+                }
+                else
+                {
+                    for (int k = 0; k < trainingInputs[i].Length; k++)
+                    {
+                        Console.WriteLine(trainingOutputs[i][0] + "\t\t\t" + net.OutputLayer[0].Output + "\t" + error[i]);
+                    }
+                }
             }
+
+            double bad = error.Where(x => x < -0.3).Count();
+            double percentage = (bad / error.Length) * 100;
+
+            return percentage;
         }
     }
 }
